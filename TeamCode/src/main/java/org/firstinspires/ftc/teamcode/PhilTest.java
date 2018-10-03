369ff9ab8eca02245506ea291d14bc9b8371c287
SootSprite15 <amelienagle@gmail.com>
2018-10-03 13:42:46 -0400

    save before rebasing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PhilTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PhilTest.java
@@ -110,27 +110,19 @@ public class PhilTest extends LinearOpMode {
                 double leftPower;
                 double rightPower;
 
-                // Choose to drive using either Tank Mode, or POV Mode
-                // Comment out the method that's not used.  The default below is POV.
-
-                // POV Mode uses left stick to go forward, and right stick to turn.
-                // - This uses basic math to combine motions and is easier to drive straight.
-
                 double drive = -gamepad1.left_stick_y;
                 double turn = gamepad1.right_stick_y;
                 leftPower = Range.clip(drive + turn, -1.0, 1.0);
                 rightPower = Range.clip(drive - turn, -1.0, 1.0);
 
-                // Tank Mode uses one stick to control each wheel.
-                // - This requires no math, but it is hard to drive forward slowly and keep straight.
-                // leftPower  = -gamepad1.left_stick_y ;
-                // rightPower = -gamepad1.right_stick_y ;
 
                 // Send calculated power to wheels
                 FrontRight.setPower(-turn);
                 FrontLeft.setPower(-drive);
                 BackLeft.setPower(drive);
                 BackRight.setPower(turn);
+
+
 
                 // Show the elapsed game time and wheel power.
                 telemetry.addData("Status", "Run Time: " + runtime.toString());
