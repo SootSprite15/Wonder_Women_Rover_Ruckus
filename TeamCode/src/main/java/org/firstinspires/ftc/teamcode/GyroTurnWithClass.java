028fff6d9184b45cb51554b0ae83a36e948d7eb1
SootSprite15 <amelienagle@gmail.com>
2018-10-10 21:28:47 -0400

    Added gyro turn function to WonderWomenRobot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTurnWithClass.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+
+@Autonomous
+public class GyroTurnWithClass extends LinearOpMode {
+    WonderWomenRobot robot = new WonderWomenRobot();
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+       robot.initRobot(hardwareMap, this);
+        while (!isStarted()) {
+            telemetry.addData("0", "robot ready");
+
+        }
+       robot.gyroTurn(90);
+       robot.gyroTurn(-90);
+
+    }
+}
+
+
