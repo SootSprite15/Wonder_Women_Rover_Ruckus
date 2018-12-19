56e8531c61868cbfeca34fae3fe10f3e54a3a5ee
SootSprite15 <amelienagle@gmail.com>
2018-12-18 21:03:15 -0500

    Fixed Extender TeleOp. Fixed LoweringAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LoweringAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LoweringAuto.java
@@ -45,7 +45,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
  */
 @Autonomous
-public class LinearArm extends LinearOpMode {
+public class LoweringAuto extends LinearOpMode {
     WonderWomenRobot robot = new WonderWomenRobot();
     @Override
     public void runOpMode() {
